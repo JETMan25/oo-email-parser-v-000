9568784ded40186c6c5bf20cc807d656aa03fab6
jontenthoff <jontenthoff@hotmail.com>
2018-09-25 14:34:11 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,6 +9,7 @@ class EmailParser
   
   def parse
     email_array = @email.split(/[ ,] /).uniq
-    email_array.reject! {|element| 
+    email_array.reject! {|element| element.empty?}
+    email_array
     
 end
